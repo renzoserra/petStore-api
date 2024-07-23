2ff9f8ee96c78a9799fd835f7581d460a5607c37
renzoserra <82285676+renzoserra@users.noreply.github.com>
2024-07-23 19:25:54 -0400

    Agrega e2e

---
+++ cypress/e2e/petStore/createUser.cy.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+
+import 'cypress-plugin-api';
+
+describe('Crear usuario', () => {
+  it('Debería crear un usuario y validar la respuesta', () => {
+    cy.api({
+      method: 'POST',
+      url: 'https://petstore.swagger.io/v2/user',
+      body: {
+        id: 999,
+        username: 'renzoserra',
+        firstName: 'Renzo',
+        lastName: 'Serra',
+        email: 'renzo@gmail.com',
+        password: '12345',
+        phone: '123456789',
+        userStatus: 1
+      }
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.code).to.eq(200);
+      expect(response.body).to.have.property('type', 'unknown');
+      expect(response.body).to.have.property('message');
+    });
+  });
+});
